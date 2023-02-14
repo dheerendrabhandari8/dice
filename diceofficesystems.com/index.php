4a8b5b4db81f0170e18f6059f6beea0849ade713
bhanu <bhanu>
2023-02-14 12:32:48 +0530

    2nd

---
+++ diceofficesystems.com/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Dice Office Systems</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+
+<table width="1004" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td colspan="4" align="left" valign="top">
+	<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+	  <tr>
+		<td width="32%" align="left" valign="top"><img src="images/logo.jpg" width="282" height="102" /></td>
+    	<td width="68%" align="right"><img src="images/logo-1N.jpg" width="322" height="102" /></td>
+	  </tr>
+	</table>	</td>
+  </tr>
+  <tr align="left" valign="top">
+    <td height="88" colspan="4" valign="top"><table width="100%" cellpadding="0" cellspacing="0" border="0">
+      <tr>
+        <td valign="middle" class="txt-11">&nbsp;</td>
+        <td height="50" valign="middle" class="txt-11"> <br />
+           </td>
+        <td align="right" valign="middle" id="topLink">&nbsp;</td>
+        <td width="68" align="right" valign="middle" id="topLink">&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="50" valign="middle" class="txt-11">&nbsp;</td>
+        <td width="282" height="38" valign="middle" class="txt-11">&nbsp;        </td>
+        <td width="604" align="right" valign="middle" id="topLink"><a href="index.php">home</a> : <a href="subcat.php?cat=21">products</a> :<a href="query.php">query</a> : <a href="contact-us.php">contact us</a></td>
+        <td width="68" align="right" valign="middle" id="topLink">&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr align="left" valign="top">
+    <td height="38" colspan="2"><img src="images/home-left-img.jpg" width="415" height="544" border="0" usemap="#Map" /></td>
+    <td class="yellowBg" width="521"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td><img src="images/heading-home.gif" width="521" height="99" /></td>
+        </tr>
+        <tr> 
+          <td valign="top" style="padding-left: 17px"> 
+            <p><strong>INTRODUCTION</strong></p>
+            <p><strong>DICE OFFICE SYSTEMS</strong> is one of the most leading 
+              company of office furniture in India with specialization in comfortable 
+              and long life <strong>CHAIRS &amp; SOFAS</strong> for all office 
+              needs of any business environment.</p>
+           </td>
+        </tr>
+        <tr> 
+          <td align="center" valign="top" style="padding-bottom: 5px"> <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="490" height="180">
+              <param name="movie" value="images/demo.swf" />
+              <param name="quality" value="high" />
+              <param name="wmode" value="transparent">
+              <embed src="images/demoN.swf" width="490" height="180" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" wmode="transparent"></embed></object> 
+          <p>&nbsp;</p></td>
+        </tr>
+        <tr> 
+          <td align="center" valign="top" id="footerLink"><a href="index.php">home</a> 
+            : <a href="subcat.php?cat=21">products 
+            range</a> : <a href="gallery.php">gallery</a> : <a href="wallpapers.php">dice 
+            wall papers</a> : <a href="query.php">online query</a> : <a href="contact-us.php">contact 
+            us</a></td>
+        </tr>
+      </table></td>
+    <td width="68">&nbsp;</td>
+  </tr>
+  <tr align="left" valign="top">
+    <td width="51" height="66">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td align="right" valign="middle" class="txt-11"> &copy; 2008 DiceOfficeSystems.com     All rights reserved</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+<map name="Map" id="Map">
+  <area shape="rect" coords="199,78,408,101" href="listsubcat.php?cat=21&amp;subcat=18" />
+<area shape="rect" coords="107,201,304,225" href="listsubcat.php?cat=21&amp;subcat=19" />
+<area shape="rect" coords="208,320,403,344" href="listsubcat.php?cat=21&amp;subcat=21" />
+<area shape="rect" coords="108,440,302,466" href="subcat.php?cat=20" />
+</map></body>
+</html>
+
